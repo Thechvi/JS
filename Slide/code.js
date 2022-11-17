c43985f128d7119130251cb7aa8209f16e2a9ff6
Vitali Chabatar <thechvi@gmail.com>
2022-11-17 19:37:37 +0100

    fiiiiiiinish

--- Slide/code.js
+++ Slide/code.js
@@ -1,14 +1,22 @@
-const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-	slide.addEventListener('click', () => {
-		clearActiveClasses();
-		slide.classList.add('action')
-	})
-}
 
-function clearActiveClasses() {
-	slides.forEach((slide) => {
-		slide.classList.remove('action')
-	})
+function slidesPlugin(activeSlide = 0) {
+	const slides = document.querySelectorAll('.slide')
+
+	slides[activeSlide].classList.add('action')
+
+	for (const slide of slides) {
+		slide.addEventListener('click', () => {
+			clearActiveClasses();
+			slide.classList.add('action')
+		})
+	}
+
+	function clearActiveClasses() {
+		slides.forEach((slide) => {
+			slide.classList.remove('action')
+		})
+	}
+
 }
+slidesPlugin(1)
